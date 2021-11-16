79f6454131c19a2b5ad602eda3a89bb9dc14f548
Franreno <franreno4@gmail.com>
2021-11-16 18:39:13 -0300

    Testing with image

--- components/irashai.js
+++ components/irashai.js
@@ -2,19 +2,42 @@ import styles from '../styles/irashai.module.css'
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import React from 'react';
+import TabPanel from '../components/tabPanel';
+import Scientific from './scientific';
 
 export default function Irashai() {
+
+    const [value, setValue] = React.useState(0);
+
+    const selectTab = (event,newValue) => {
+        setValue(newValue);
+    }
+
     return (
         <>
         <div className={styles.mainContainer}>
             <h2 className={styles.title}>いらっしゃい</h2>
 
             <AppBar position="static" style={{background: '#3f2372'}}>
-                <Tabs centered>
+                <Tabs centered value={value} onChange={selectTab}>
+                    <Tab label="About"/>
                     <Tab label="Research Projects"/>
                     <Tab label="Blog"/>
                 </Tabs>
             </AppBar>
+
+            <TabPanel index={0} value={value}>
+                <h3>Teste About</h3>
+            </TabPanel>
+
+            <TabPanel index={1} value={value}>
+                <Scientific/>
+            </TabPanel>
+
+            <TabPanel index={2} value={value}>
+                <h3>Teste Blog</h3>
+            </TabPanel>
 
         </div>
     
---
+++ components/scientific.js
@@ -0,0 +1,10 @@
+import styles from '../styles/scientific.module.css'
+
+export default function Scientific() {
+    return (
+        <>
+        <img src={"images/Rio.png"} className={styles.img}></img>
+        <h3>Text</h3>
+        </>
+    )
+}
