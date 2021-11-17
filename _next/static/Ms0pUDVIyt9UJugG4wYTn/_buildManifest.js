da432bbbd6bc3b358891db2f977b2e479f5fc31d
Franreno <67326251+Franreno@users.noreply.github.com>
2021-11-17 01:49:38 +0000

    Deploying to gh-pages from @ Franreno/franreno.github.io@e0cdc0438dfe5ac1695d4c24231ac328d09ccfb1 🚀

---
+++ _next/static/Ms0pUDVIyt9UJugG4wYTn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/191-c13b7605988a322c.js","static/css/5ad12c3901e52638.css","static/chunks/pages/index-af9a4f8cb2271bed.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
